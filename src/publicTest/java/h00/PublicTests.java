9ef52b14dd930f739b0bc9199214736451aa530a
Alexander Staeding <alexanderstaeding@gmail.com>
2022-04-12 00:09:09 +0200

    Fix formatting

--- src/publicTest/java/h00/PublicTests.java
+++ src/publicTest/java/h00/PublicTests.java
@@ -1,9 +1,9 @@
 package h00;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Public Tests")
 public class PublicTests {
